01dc4714d5fcbd1deeb1d6736c4569bea6b5f1c0
Adrian <krawczyk.25@vp.pl>
2019-12-15 10:33:04 +0100

    [init] - init

--- src/main/java/pl/projekt/springwithgit/UserController.java
+++ src/main/java/pl/projekt/springwithgit/UserController.java
@@ -1,7 +1,15 @@
 package pl.projekt.springwithgit;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
 
 
+    @GetMapping("/home")
+public String getUser(){
+    return "home";
+}
 
 }
