bbaf90ab2676ea0cdaf1a54ded05d5726d54d9d8
Adrian <krawczyk.25@vp.pl>
2019-12-15 12:32:15 +0100

    [change] - repository method add

--- src/main/java/pl/projekt/springwithgit/repo/AddressRepository.java
+++ src/main/java/pl/projekt/springwithgit/repo/AddressRepository.java
@@ -3,5 +3,8 @@ package pl.projekt.springwithgit.repo;
 import org.springframework.data.repository.CrudRepository;
 import pl.projekt.springwithgit.Domain.Address;
 
+import java.util.List;
+
 public interface AddressRepository extends CrudRepository<Address, Long> {
+    List<Address> findAllOrderByCity();
 }
